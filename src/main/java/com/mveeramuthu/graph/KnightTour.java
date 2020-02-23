10e5aedee3b85d8bcb8fed037491348c231a7219
Mohan Veeramuthu <mohan.veeramuthu@ask.com>
2020-02-22 19:47:37 -0800

    [WIP] graph -> knight's tour : implementation

--- src/main/java/com/mveeramuthu/graph/KnightTour.java
+++ src/main/java/com/mveeramuthu/graph/KnightTour.java
@@ -1,7 +1,6 @@
 package com.mveeramuthu.graph;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Knight's Tour On A Chess Board
@@ -47,31 +46,79 @@ import java.util.Queue;
  *
  */
 
-public class KnightTour {
-    
+
+public class KnightTour { 
+    /*
+     Approach: Graph BFS
+     T: O(rows * cols)
+     S: O(rows * cols)
+    */
     public static int findMinMoves(int rows, int cols, 
                                    int startRow, int startCol, 
-                                   int endRow, int endCol) { 
-            /*
-                 Approach: Graph BFS
-                 T: O(rows * cols)
-                 S: O(rows * cols)
-                 
-                // x and y direction, where a knight can move  
-                // queue for storing states of knight in board  
-                // make all cell unvisited  
-                // visit starting state  
-                // loop untill we have one element in queue  
-                // if current cell is equal to target cell, return its distance
-                // loop for all reachable states  
-                // If reachable state is not yet visited and inside board, push that state into queue
-            */
+                                   int endRow, int endCol) {
+        // if starting and ending positions are same
+        if (startRow == endRow && startCol == endCol) {
+            return 0;
+        }
         
+        // cell visited status and distance to target node
+        int[][] distance  = new int[rows][cols];
+        
+        // starting position can be reached from starting position in 0 moves
+        distance[startRow][startCol] = 0; 
+
+        // BFS queue
         Queue<Cell> queue = new LinkedList<>();
+
+        // knight's possible moves
+        List<Cell> directions = new ArrayList<>(
+                Arrays.asList(
+                        new Cell(1, 2),
+                        new Cell(2, 1),
+                        new Cell(-1, 2),
+                        new Cell(-2, 1),
+                        new Cell(1, -2),
+                        new Cell(2, -1),
+                        new Cell(-1, -2),
+                        new Cell(-2, -1)
+                )
+        );
+
+        // initial state of the cells in the chessboard
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                distance[i][j] = -1;
+            }
+        }
         
+        // loop until queue is empty
+        while(!queue.isEmpty()) {
+            Cell currCell = queue.poll();
+            
+            // if current cell is equal to target cell, return its distance
+            if(currCell.x == endRow && currCell.y == endCol) {
+                return distance[endRow][endCol];
+            }
+
+            for(Cell direction: directions) {
+                int x = currCell.x + direction.x;
+                int y = currCell.y + direction.y;
+                
+                // boundry conditions
+                if(x < startRow || x >= endRow 
+                        || y < startCol || y >= endCol
+                        || distance[x][y] <= distance[currCell.x][currCell.y]+1) {
+                    continue;
+                }
+
+                // if reachable cell is not yet visited and is inside board, push that state into queue
+                queue.offer(new Cell(x, y));
+                // increment distance 
+                distance[x][y] = distance[currCell.x][currCell.y] + 1;
+            }
+        }
         
-        
-        
+        // If ending position can not be reached from starting position
         return -1;
     }
 }
--- src/test/java/com/mveeramuthu/graph/KnightTourTest.java
+++ src/test/java/com/mveeramuthu/graph/KnightTourTest.java
@@ -2,10 +2,17 @@ package com.mveeramuthu.graph;
 
 import org.junit.Test;
 
-public class KnightTourTest {
-    
+import static junit.framework.TestCase.assertEquals;
+
+public class KnightTourTest { 
     @Test
-    public void findMinMovesTest() {
-        
+    public void findMinMovesFirstQuadrantPositiveTest() { 
+        int observedResult = KnightTour.findMinMoves(5, 5, 0, 0, 4, 1);
+        assertEquals (3, observedResult);
+    }
+    @Test
+    public void findMinMovesFirstQuadrantNegativeTest() {
+        int observedResult = KnightTour.findMinMoves(5, 5, 0, 0, 3, 1);
+        assertEquals (-1, observedResult);
     }
 }
