5c814b5b63e77cf0abd9df9579ff0b69eeeccc4d
Mohan Veeramuthu <mohan.veeramuthu@ask.com>
2020-03-10 17:46:25 -0700

    stash

---
+++ src/main/java/com/mveeramuthu/ds/Pair.java
@@ -0,0 +1,23 @@
+package com.mveeramuthu.ds;
+
+public class Pair<N, V> {
+    private final N first;
+    private final V second;
+
+    public Pair(N first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public N getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public static <N, V> Pair<N, V> pair(N name, V value) {
+        return new Pair<>(name, value);
+    }
+}
