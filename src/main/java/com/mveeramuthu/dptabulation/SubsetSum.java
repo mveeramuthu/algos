f98c3c2635be0aebd5bb54478ab1a4682b453673
Mohan Veeramuthu <msrockzzz@gmail.com>
2020-09-30 23:57:16 -0700

    dp - subset sum - tabulation

--- src/main/java/com/mveeramuthu/dptabulation/SubsetSum.java
+++ src/main/java/com/mveeramuthu/dptabulation/SubsetSum.java
@@ -3,36 +3,26 @@ package com.mveeramuthu.dptabulation;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-public class EqualSubsetSum {
-    public static boolean canEqualSubsetSum(int[] num) {
+public class SubsetSum {
+    public static boolean canSubsetSum(int[] num, int sum) {
         if(num == null || num.length == 0) {
             return false;
         }
         
         /*
-            row = sum | 0 <= rowLen <= sum/2
+            row = sum | 0 <= rowLen <= sum
             col = num | 0 <= colLen <= num.length
          */
-        int colLen = num.length;
+        int numLen = num.length;
+        boolean[][] dp = new boolean[numLen][sum + 1];
         
-        int totalSum = 0;
-        for(int i=0; i<colLen; i++) {
-            totalSum += num[i];
-        }
-        // odd number total sum => equal subset sum impossible
-        if(totalSum % 2 != 0) {
-            return false;
-        }
-            
-        int requiredSum = totalSum/2;
-        
-        boolean[][] dp = new boolean[colLen][requiredSum + 1];
         // initialize first col
-        for(int i=0; i<colLen; i++) {
+        for(int i=0; i<numLen; i++) {
             dp[i][0] = true; // sum = 0 is possible with empty set 
         }
+        
         // initialize first row
-        for(int j=1; j<=requiredSum; j++) {
+        for(int j=1; j<=sum; j++) {
             /*
                 * one num is 0 and the other num is the current value
                 * required sum = sum of these 2 numbers = the other num
@@ -49,13 +39,13 @@ public class EqualSubsetSum {
         Stream.of(dp).map(Arrays::toString).forEach(System.out::println);
         
         // build
-        for(int i=1; i<colLen; i++) {
-            for(int j=1; j<=requiredSum; j++) {
+        for(int i=1; i<numLen; i++) {
+            for(int j=1; j<=sum; j++) {
                 // exclude num[i]
                 if(dp[i - 1][j]) {
                     dp[i][j] = dp[i - 1][j];
-                } else if(requiredSum >= num[i]) { // include num[i]
-                    dp[i][j] = dp[i - 1][requiredSum - num[i]];
+                } else if(sum >= num[i]) { // include num[i]
+                    dp[i][j] = dp[i - 1][sum - num[i]];
                 } 
             }
         }
@@ -63,11 +53,15 @@ public class EqualSubsetSum {
         System.out.println();
         Stream.of(dp).map(Arrays::toString).forEach(System.out::println);
         
-        return dp[colLen - 1][requiredSum];
+        return dp[numLen - 1][sum];
     }
     
     public static void main(String args[]) {
-        int[] num = {1, 2, 3, 4};
-        System.out.println(canEqualSubsetSum(num));
+        int[] num = {1, 2, 3, 7};
+        System.out.println(canSubsetSum(num, 6));
+        num = new int[] { 1, 2, 7, 1, 5 };
+        System.out.println(canSubsetSum(num, 10));
+        num = new int[] { 1, 3, 4, 8 };
+        System.out.println(canSubsetSum(num, 6));
     }
 }
