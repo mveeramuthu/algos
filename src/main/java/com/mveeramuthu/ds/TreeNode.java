e3e4ddcef80c53a1adf502a2d1addb4097c8960f
Mohan Veeramuthu <msrockzzz@gmail.com>
2020-10-03 01:24:48 -0700

    tree - lot

--- src/main/java/com/mveeramuthu/ds/TreeNode.java
+++ src/main/java/com/mveeramuthu/ds/TreeNode.java
@@ -1,10 +1,18 @@
 package com.mveeramuthu.ds;
 
 public class TreeNode {
+    int value;
     TreeNode left;
     TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;        
+    }
     
-    public TreeNode(TreeNode left, TreeNode right) {
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
         this.left = left;
         this.right = right;
     }
--- src/main/java/com/mveeramuthu/trees/bfs/LevelOrderTraversal.java
+++ src/main/java/com/mveeramuthu/trees/bfs/LevelOrderTraversal.java
@@ -23,6 +23,11 @@ public class LevelOrderTraversal {
     }
     
     public static void main(String args[]) {
-        
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
     }
 }
