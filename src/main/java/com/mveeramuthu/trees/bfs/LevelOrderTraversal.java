5580c56bdab8da45e02bf509b45664a10e953cab
Mohan Veeramuthu <msrockzzz@gmail.com>
2020-10-03 01:32:41 -0700

    tree - lot

--- src/main/java/com/mveeramuthu/trees/bfs/LevelOrderTraversal.java
+++ src/main/java/com/mveeramuthu/trees/bfs/LevelOrderTraversal.java
@@ -2,24 +2,31 @@ package com.mveeramuthu.trees.bfs;
 
 import com.mveeramuthu.ds.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LevelOrderTraversal {
-    public void bfs(TreeNode root) {
+    public static List<List<Integer>> bfs(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
         if(root == null) {
-            return;
+            return result;
         }
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         
+        /*
         while(!queue.isEmpty()) {
             
             
             
         }
         
+         */
+        
+        return result;
     }
     
     public static void main(String args[]) {
@@ -29,5 +36,7 @@ public class LevelOrderTraversal {
         root.left.left = new TreeNode(9);
         root.right.left = new TreeNode(10);
         root.right.right = new TreeNode(5);
+
+        System.out.println(bfs(root));
     }
 }
