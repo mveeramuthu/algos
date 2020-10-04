0606b62a04f27debfbf24504b3b650a3174d867a
Mohan Veeramuthu <msrockzzz@gmail.com>
2020-10-04 07:15:14 -0700

    Bottom-up level order traversal

--- src/main/java/com/mveeramuthu/trees/bfs/BottomUpLevelOrderTraversal.java
+++ src/main/java/com/mveeramuthu/trees/bfs/BottomUpLevelOrderTraversal.java
@@ -1,6 +1,59 @@
 package com.mveeramuthu.trees.bfs;
 
+/*
+    Use stack or reverse while retrieving values from queue
+ */
+
+import com.mveeramuthu.ds.TreeNode;
+
+import java.util.*;
+
 public class BottomUpLevelOrderTraversal {
-    
-    
+    public static List<List<Integer>> reverseBfs(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        Stack<List<Integer>> stack = new Stack<>();
+
+        while(!queue.isEmpty()) {
+            List<Integer> currLevel = new ArrayList<>();
+            int queueSize = queue.size();
+
+            for(int i=0; i<queueSize; i++) {
+                TreeNode currNode = queue.poll();
+
+                currLevel.add(currNode.value);
+                if (currNode.left != null) {
+                    queue.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    queue.add(currNode.right);
+                }
+            }
+
+            stack.add(currLevel);
+        }
+        
+        while(!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        
+        return result;
+    }
+
+    public static void main(String args[]) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+
+        System.out.println(reverseBfs(root));
+    }
 }
