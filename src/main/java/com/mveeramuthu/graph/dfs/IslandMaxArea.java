21bc3f70a2e7ecb1118c3c6214047c9e0d52d517
Mohan Veeramuthu <msrockzzz@gmail.com>
2020-11-08 19:16:49 -0800

    graph dfs - IslandMaxArea

--- src/main/java/com/mveeramuthu/graph/dfs/IslandMaxArea.java
+++ src/main/java/com/mveeramuthu/graph/dfs/IslandMaxArea.java
@@ -1,17 +1,51 @@
 package com.mveeramuthu.graph.dfs;
 
-import java.util.Objects;
-
 public class IslandMaxArea {
+    public static int getMaxArea(int[][] grid) {
+        int maxArea = Integer.MIN_VALUE;
+        if(grid == null || grid.length == 0) {
+            return maxArea;
+        }
 
-    public static int maxArea(int[][] grid) {
-        Objects.requireNonNull(grid);
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean[][] visited = new boolean[row+1][col+1];
 
-        return -1;
+        for (int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                maxArea = Math.max(maxArea, dfs(grid, visited, i, j));
+            }
+        }
 
+        return maxArea;
+    }
+
+    private static int dfs(int[][] grid, boolean[][] visited, int row, int col) {
+        if(row < 0 || row >= grid.length
+            || col < 0 || col >= grid[0].length
+            || visited[row][col]
+            || grid[row][col] == 0) {
+            return 0;
+        }
+
+        visited[row][col] = true;
+
+        return dfs(grid, visited, row-1, col) +
+                dfs(grid, visited, row, col-1) +
+                dfs(grid, visited, row, col+1) +
+                dfs(grid, visited, row+1, col) + 1;
     }
 
     public static void main(String args[]) {
-
+        System.out.println(getMaxArea(new int[][]{
+            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        }));
     }
 }
--- src/main/java/com/mveeramuthu/graph/dfs/ZombieClusters.java
+++ src/main/java/com/mveeramuthu/graph/dfs/ZombieClusters.java
@@ -5,18 +5,18 @@ Zombie Clusters
 
 
 Problem Statement:
-There are zombies in Seattle. Liv and Ravi are trying to track them down to find out 
-who is creating new zombies in an effort to prevent an apocalypse. Other than the patient-zero zombies 
-(who became so by mixing MaxRager and tainted Utopium), new people only become zombies after being 
-scratched by an existing zombie. Zombiism is transitive. This means that if zombie 0 knows zombie 1 
-and zombie 1 knows zombie 2, then zombie 0 is connected to zombie 2 by way of knowing zombie 1. 
-A zombie cluster is a group of zombies who are directly or indirectly linked through the other zombies 
+There are zombies in Seattle. Liv and Ravi are trying to track them down to find out
+who is creating new zombies in an effort to prevent an apocalypse. Other than the patient-zero zombies
+(who became so by mixing MaxRager and tainted Utopium), new people only become zombies after being
+scratched by an existing zombie. Zombiism is transitive. This means that if zombie 0 knows zombie 1
+and zombie 1 knows zombie 2, then zombie 0 is connected to zombie 2 by way of knowing zombie 1.
+A zombie cluster is a group of zombies who are directly or indirectly linked through the other zombies
 they know, such as the one who scratched them or supplies who them with brains.
 
-We have a 2D array having n rows and n columns where each cell, zombies[A][B], denotes whether zombie A 
-knows zombie B. The diagram showing connectedness will be made up of a number of binary strings, characters 0 or 1. 
-Each of the characters in the string represents whether the zombie associated with a row element is connected to 
-the zombie at that character's index. For instance, a zombie 0 with a connectedness string '110' is connected to 
+We have a 2D array having n rows and n columns where each cell, zombies[A][B], denotes whether zombie A
+knows zombie B. The diagram showing connectedness will be made up of a number of binary strings, characters 0 or 1.
+Each of the characters in the string represents whether the zombie associated with a row element is connected to
+the zombie at that character's index. For instance, a zombie 0 with a connectedness string '110' is connected to
 zombies 0 (itself) and zombie 1, but not to zombie 2. The complete matrix of zombie connectedness is:
 
 110
@@ -24,7 +24,7 @@ zombies 0 (itself) and zombie 1, but not to zombie 2. The complete matrix of zom
 001
 
 Zombies 0 and 1 are connected. Zombie 2 is not. Your task is to determine the number of connected groups of zombies, or clusters, in a given matrix.
-on-Find data structure with Union By Rank and Path Compression techniques may pass, but is not acceptable. 
+on-Find data structure with Union By Rank and Path Compression techniques may pass, but is not acceptable.
 These issues should go away when we have a more nuanced backend.
 */
 
@@ -33,13 +33,13 @@ import com.mveeramuthu.ds.AdjacencyMatrixDirectedGraph;
 import java.util.List;
 
 public class ZombieClusters {
-    
+
     public static int getZombieClustersCount(List<String> input) {
         int N = Integer.parseInt(input.get(0));
-                
-        // create adjacency matrix  
+
+        // create adjacency matrix
         AdjacencyMatrixDirectedGraph graph = new AdjacencyMatrixDirectedGraph(N, N);
-        
+
         int k = 1;
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
@@ -47,12 +47,16 @@ public class ZombieClusters {
                 k++;
             }
         }
-        
+
         graph.print(graph);
-        
+
         // perform dfs
-        
-        
+
+
         return -1;
+    }
+
+    private static void dfs(AdjacencyMatrixDirectedGraph graph) {
+
     }
 }
